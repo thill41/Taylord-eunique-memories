81e8a7c2ef35a0eb774b6c9a90656cbb16e8882c
Vell McIlwain <vmcilwain@me.com>
2024-09-14 11:47:41 -0400

    add controller tests

--- test/controllers/abouts_controller_test.rb
+++ test/controllers/abouts_controller_test.rb
@@ -1,14 +1,39 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class AboutsControllerTest < ActionDispatch::IntegrationTest
-#   def setup
-#     @about = create(:about)
-#   end
+class AboutsControllerTest < BaseIntegrationTest
+  def setup
+    @user = create(:user)
+    @about = create(:about)
+  end
 
-#   test 'GET show' do
-#     get about_url(@about)
+  test 'GET show' do
+    get abouts_url
 
-#     assert_response :success
-#     assert_select 'h1', 'About Us'
-#   end
-# end
+    assert_response :success
+  end
+
+  test 'GET edit' do
+    sign_in @user
+
+    get edit_abouts_url
+
+    assert_response :success
+  end
+
+  test 'PATCH update success' do
+    sign_in @user
+
+    patch abouts_url, params: { about: { content: 'New content' } }
+    assert_flash(:success)
+    assert_redirected_to abouts_url
+  end
+
+  test 'PATCH update failure' do
+    sign_in @user
+
+    patch abouts_url, params: { about: { content: '' } }
+    
+    assert_response :success
+    assert_flash(:error)
+  end
+end
