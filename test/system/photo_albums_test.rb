804200b36cbd2c47b868155c19675f37e19be3f6
Vell McIlwain <vmcilwain@me.com>
2024-09-15 09:33:08 -0400

    some basic formatting

---
+++ test/system/photo_albums_test.rb
@@ -0,0 +1,9 @@
+require 'application_system_test_case'
+
+class PhotoAlbumsTest < ApplicationSystemTestCase
+  # test 'visiting the index' do
+  #   visit photo_albums_url
+  #
+  #   assert_selector 'h1', text: 'PhotoAlbums'
+  # end
+end
