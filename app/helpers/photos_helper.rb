74654edbdbc9995087014b74f28bbeffa4a69602
Vell McIlwain <vmcilwain@me.com>
2024-09-15 16:43:07 -0400

    fix cancel button helper method

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,5 @@
 module PhotosHelper
+  def cancel_button(photo)
+    link_to 'Cancel', (photo.persisted? ? photo_album_photo_path(photo) : photo_album_path(photo.photo_album)), class: 'btn btn-secondary'
+  end
 end
