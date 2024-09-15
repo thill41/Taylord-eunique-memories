25e9e65fcef4a85c4355d9d998b1fac211494b96
Vell McIlwain <vmcilwain@me.com>
2024-09-15 16:42:30 -0400

    add photo_actions method

--- app/helpers/photo_albums_helper.rb
+++ app/helpers/photo_albums_helper.rb
@@ -8,4 +8,12 @@ module PhotoAlbumsHelper
   def cancel_button(photo_album)
     link_to 'Cancel', (photo_album.persisted? ? photo_album_path(photo_album) : photo_albums_path), class: 'btn btn-secondary'
   end
+
+  def photo_actions(photo)
+    return unless user_signed_in?
+    
+    tag.div class: 'actions' do
+      "#{link_to 'Edit Photo', edit_photo_album_photo_path(photo.photo_album, photo)} #{button_to 'Delete Photo', photo_album_photo_path(photo.photo_album, photo), method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-danger'}".html_safe
+    end
+  end
 end
