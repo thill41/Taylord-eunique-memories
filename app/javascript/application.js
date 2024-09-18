b04a8456bac187f6fbe3f5bf213681bc5c8dadd3
Vell McIlwain <vmcilwain@me.com>
2024-09-18 19:11:14 -0400

    remove the rest of lightbox2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,5 +9,3 @@ import "@popperjs/core";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "bootstrap"; // Import Bootstrap JS
 import * as bootstrap from "bootstrap"
-import 'lightbox2/dist/js/lightbox-plus-jquery'
-import 'lightbox2/dist/css/lightbox.css'
