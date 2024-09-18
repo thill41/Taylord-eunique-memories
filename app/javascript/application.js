aae094461e3ba32b72070d3b584f684b7d433344
Vell McIlwain <vmcilwain@me.com>
2024-09-17 20:51:46 -0400

    add lightbox2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,3 +9,5 @@ import "@popperjs/core";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "bootstrap"; // Import Bootstrap JS
 import * as bootstrap from "bootstrap"
+import 'lightbox2/dist/js/lightbox-plus-jquery'
+import 'lightbox2/dist/css/lightbox.css'
