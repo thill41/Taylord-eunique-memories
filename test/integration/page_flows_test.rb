114ed64b7425c474a0bb0f9a01960b482b31e071
Vell McIlwain <vmcilwain@me.com>
2024-09-24 16:12:01 -0400

    update homepage tests

--- test/integration/page_flows_test.rb
+++ test/integration/page_flows_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class PagFlowsTest < BaseIntegrationTest
-  test 'Get /' do
+  test 'GET /' do
     get '/'
     
     assert_select 'h2', 'Our Mission: Delivering Excellence Every Day'
+    assert_select 'a', text: 'Manage Packages', count: 0
+    assert_select 'a', text: 'Manage Photo Albums', count: 0
+
+    sign_in create(:user)
+    get '/'
+    assert_select 'a', text: 'Manage Packages'
+    assert_select 'a', text: 'Manage Photo Albums'
   end
 end
