1c03c35e7f44a4df4403163d97b3debf9134f711
Vell McIlwain <vmcilwain@me.com>
2024-09-28 14:37:34 -0400

    remove duplicate code

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
   Stripe.api_key = Rails.application.credentials.stripe[:stripe_secret_key]
-  Stripe.api_key = Rails.application.credentials.stripe[:stripe_publishable_key]
 else
   Stripe.api_key = ENV.fetch('STRIPE_SECRET_KEY', nil)
 end
