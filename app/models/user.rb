2de4b6483758a193a2b3c07585ae5d1396bf7444
Vell McIlwain <vmcilwain@me.com>
2024-09-29 20:48:12 -0400

    remove user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :registerable
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable, :confirmable
   validates :first_name, :last_name, :username, presence: true
   validates :username, uniqueness: { case_sensitive: false }
