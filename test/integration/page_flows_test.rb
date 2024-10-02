0d7753de090391ee08e54ad64b14216a41c2f1e7
Vell McIlwain <vmcilwain@me.com>
2024-10-01 20:30:03 -0400

    update page_flows_test.rb

--- test/integration/page_flows_test.rb
+++ test/integration/page_flows_test.rb
@@ -3,18 +3,29 @@ require 'test_helper'
 class PageFlowsTest < BaseIntegrationTest
   setup do
     create(:about)
-    create(:mission_statement)
+    @mission_statement = create(:mission_statement)
   end
 
   test 'GET /' do
     get '/'
     
-    assert_select 'h3', 'About'
+    assert_select 'h2', 'About'
+    assert_select 'a', text: 'Sign In'
+    assert_select 'a', text: 'Sign Out', count: 0
+    assert_select 'a', text: 'Profile', count: 0
+    assert_select 'a', text: 'Manage About', count: 0
+    assert_select 'a', text: 'Manage Mission Statement', count: 0
     assert_select 'a', text: 'Manage Packages', count: 0
     assert_select 'a', text: 'Manage Photo Albums', count: 0
 
-    sign_in create(:user)
+    sign_in @mission_statement.user
     get '/'
+
+    assert_select 'a', text: 'Sign In', count: 0
+    assert_select 'a', text: 'Sign Out'
+    assert_select 'a', text: 'Profile'
+    assert_select 'a', text: 'Manage About'
+    assert_select 'a', text: 'Manage Mission Statement'
     assert_select 'a', text: 'Manage Packages'
     assert_select 'a', text: 'Manage Photo Albums'
   end
